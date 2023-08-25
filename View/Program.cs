63e663e8684c26598c603b43e504eb7f03301bfe
Douglas Modena <d.modena@live.com>
2023-08-25 17:28:02 -0300

    Simple workflow functional

--- View/Program.cs
+++ View/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.Text;
+using Game;
 
-void PrintBoard(int[,] values, string[] symbols)
+static void PrintBoard(uint[,] values, string[] symbols)
 {
     var sb = new StringBuilder();
 
@@ -13,7 +14,63 @@ void PrintBoard(int[,] values, string[] symbols)
     Console.WriteLine(sb.ToString());
 }
 
-int[,] values = { { 1, 0, 0 }, { 0, 2, 0 }, { 0, 0, 0 } };
+static uint? GetUserPosition()
+{
+    bool validInput = false;
+
+    do
+    {
+        Console.Write("\nChoose position (1-9): ");
+        var input = Console.ReadLine();
+
+        var parsed = uint.TryParse(input, out uint inputValue);
+        if (!parsed)
+            continue;
+
+        if (inputValue < 1 || inputValue > 9)
+            continue;
+
+        return inputValue - 1u;
+
+    } while (!validInput);
+
+    return null;
+}
+
+//int[,] values = { { 1, 0, 0 }, { 0, 2, 0 }, { 0, 0, 0 } };
+//var symbols = new string[] { " ", "x", "o" };
+
+//PrintBoard(values, symbols);
+
+var gameControl = new Control();
+var cpu = new PredictablePlayer();
 var symbols = new string[] { " ", "x", "o" };
 
-PrintBoard(values, symbols);
+
+Console.Write("Would you like to start? (Y/n): ");
+var input = Console.ReadLine();
+var humanFirst = input?.ToLower().Trim() == "y";
+
+var (humanValue, cpuValue) = (1u, 2u);
+uint? pos;
+
+if (!humanFirst)
+{
+    pos = cpu.Play(gameControl.Values, cpuValue);
+    if (pos != null)
+        gameControl.SetValue(pos.Value, cpuValue);
+}
+
+do
+{
+    Console.WriteLine();
+    PrintBoard(gameControl.Values, symbols);
+    pos = GetUserPosition();
+    if (pos != null)
+        gameControl.SetValue(pos.Value, humanValue);
+
+    pos = cpu.Play(gameControl.Values, cpuValue);
+    if (pos != null)
+        gameControl.SetValue(pos.Value, cpuValue);
+
+} while (gameControl.Active);
