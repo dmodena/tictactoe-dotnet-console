54f9ec9df4b14d83a50475df26e9076a107bc638
Douglas Modena <d.modena@live.com>
2023-08-25 16:04:53 -0300

    Add initial tests for Game.Control

---
+++ Game.Tests/ControlTests.cs
@@ -0,0 +1,69 @@
+using Game;
+
+namespace Game.Tests;
+
+public class ControlTests
+{
+    private Control _sut;
+
+    public ControlTests()
+    {
+        _sut = new Control();
+    }
+
+    [Theory]
+    [InlineData(-999)]
+    [InlineData(-1)]
+    [InlineData(9)]
+    [InlineData(999)]
+    public void SetValue_ReturnsFalse_WhenInvalidPosition(int position)
+    {
+        var value = 1;
+
+        var result = _sut.SetValue(position, value);
+
+        Assert.False(result);
+    }
+
+    [Theory]
+    [InlineData(-999)]
+    [InlineData(-1)]
+    [InlineData(3)]
+    [InlineData(999)]
+    public void SetValue_ReturnsFalse_WhenInvalidValue(int value)
+    {
+        var position = 1;
+
+        var result = _sut.SetValue(position, value);
+
+        Assert.False(result);
+    }
+
+    [Theory]
+    [InlineData(1, 1)]
+    [InlineData(1, 2)]
+    [InlineData(2, 1)]
+    [InlineData(2, 2)]
+    public void SetValue_ReturnsFalse_WhenValueAlreadySet
+        (int initialValue, int newValue)
+    {
+        var position = 1;
+        _sut.SetValue(position, initialValue);
+
+        var result = _sut.SetValue(position, newValue);
+
+        Assert.False(result);
+    }
+
+    [Theory]
+    [InlineData(1, 2)]
+    [InlineData(2, 1)]
+    [InlineData(5, 1)]
+    [InlineData(8, 2)]
+    public void SetValue_ReturnsTrue_WhenValueSet(int position, int value)
+    {
+        var result = _sut.SetValue(position, value);
+
+        Assert.True(result);
+    }
+}
---
+++ Game/Control.cs
@@ -0,0 +1,44 @@
+﻿namespace Game;
+public class Control
+{
+
+    public bool Active { get; private set; }
+    private int[,] Values;
+
+    private const int MinValue = 0;
+    private const int MaxValue = 2;
+    private const int MinPosition = 0;
+    private const int MaxPosition = 8;
+
+    public Control()
+    {
+        Values = new int[3, 3];
+        Active = true;
+    }
+
+
+    public bool SetValue(int position, int value)
+    {
+        if (position < MinPosition || position > MaxPosition)
+            return false;
+        if (value < MinValue || value > MaxValue)
+            return false;
+
+        var (row, col) = MapPosition(position);
+        var current = Values[row, col];
+
+        if (current != 0)
+            return false;
+
+        Values[row, col] = value;
+        return true;
+    }
+
+    private (int, int) MapPosition(int inPosition)
+    {
+        var row = inPosition / 3;
+        var col = inPosition % 3;
+
+        return (row, col);
+    }
+}
